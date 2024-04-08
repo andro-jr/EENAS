ceefa9c88fe5d7c8789a883da908797851a84021
andro-jr <prabin20panta@gmail.com>
2024-04-08 20:11:27 +0545

    album

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ $(".product-carousel").owlCarousel({
 
     }
 });
+
+
+// Lightbox
+
+$(document).ready(function () {
+    $(".popup-gallery").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        },
+    });
+});
